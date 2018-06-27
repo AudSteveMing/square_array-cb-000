0d99b3baf227935698e595b990a25d812bbc275f
Steve Cordova <cordovas@bc.edu>
2018-06-27 20:17:23 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
   x = []
-#   array.each do |array_content| {
-#     squared = array_content * array_content
-#     x.push(squared)
-#   }
-# end
+  array.each do |array_content| 
+    squared = array_content * array_content
+    x.push(squared)
+
+end
   x
 end
